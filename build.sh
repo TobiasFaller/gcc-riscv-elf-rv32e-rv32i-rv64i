ff887c6435b0f6af7588f8ea6bbae595f1162421
Tobias Faller <TobiasFaller@gmx.net>
2019-01-15 01:20:57 +0100

    Updated Makefile structure; Added re-entry feature

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -e
 __OPT_MULTICORE=-j4
 __OPT_BOOTSTRAP=--enable-bootstrap
 __OPT_TARGET_PATH=/usr/local/riscv32-unknown-elf
+__OPT_TARGET_PREFIX=riscv32-unknown-elf-
 __OPT_TARGET_ARCH=riscv32-unknown-elf
 __OPT_TARGET_MARCH=rv32i
 __OPT_TARGET_MARCH_FULL=rv32imafc
@@ -15,99 +16,168 @@ __OPT_TARGET_MULTILIB="rv32e-ilp32e-- rv32ec-ilp32e-- rv32em-ilp32e-- rv32emc-il
   rv32if-ilp32-- rv32icf-ilp32-- rv32im-ilp32-- rv32imc-ilp32--
   rv32imaf-ilp32-- rv32imafc-ilp32-- rv32iaf-ilp32-- rv32iafc-ilp32--"
 
+__ROOT_DIR=`pwd`
+
 export PATH=$PATH:${__OPT_TARGET_PATH}/bin
 
-git clone --config core.autocrlf=input --depth=1 --branch=riscv-next https://github.com/riscv/riscv-binutils-gdb.git riscv-binutils
-git clone --config core.autocrlf=input --depth=1 --branch=riscv-next https://github.com/riscv/riscv-gcc.git riscv-gcc
-git clone --config core.autocrlf=input --depth=1 --branch=riscv-newlib-next https://github.com/riscv/riscv-newlib.git riscv-newlib
-git clone --config core.autocrlf=input --depth=1 --branch=master git://git.busybox.net/uClibc++ uclibc++
+(cd riscv-binutils && git pull origin master) \
+  || git clone --config core.autocrlf=input --depth=1 --branch=riscv-next https://github.com/riscv/riscv-binutils-gdb.git riscv-binutils
+(cd riscv-gcc && git pull origin master) \
+  || git clone --config core.autocrlf=input --depth=1 --branch=riscv-next https://github.com/riscv/riscv-gcc.git riscv-gcc
+(cd riscv-newlib && git pull origin master) \
+  || git clone --config core.autocrlf=input --depth=1 --branch=riscv-newlib-next https://github.com/riscv/riscv-newlib.git riscv-newlib
+(cd riscv-uclibc++ && git pull origin master) \
+  || git clone --config core.autocrlf=input --depth=1 --branch=master git://git.busybox.net/uClibc++ riscv-uclibc++
 
-mkdir build-binutils && cd build-binutils
-../riscv-binutils/configure \
---target=${__OPT_TARGET_ARCH} \
---prefix=${__OPT_TARGET_PATH} \
---with-arch=${__OPT_TARGET_MARCH_FULL} \
---enable-lto \
---disable-nls --disable-wchar_t \
---enable-initfini-array
-make all ${__OPT_MULTICORE}
-make install ${__OPT_MULTICORE}
-cd ..
+# ----------------------------------------------------------------------------
+# binutils
+# ----------------------------------------------------------------------------
 
-mv ./riscv-gcc/gcc/config/riscv/t-elf-multilib ./riscv-gcc/gcc/config/riscv/t-elf-multilib64 | true
-./riscv-gcc/gcc/config/riscv/multilib-generator ${__OPT_TARGET_MULTILIB} \
-  > ./riscv-gcc/gcc/config/riscv/t-elf-multilib32
-patch ./riscv-gcc/gcc/config.gcc ./config.gcc.patch
+cd $__ROOT_DIR
+if [ ! -d built-binutils ]; then
+  rm -r build-binutils || true
+  mkdir build-binutils && cd build-binutils
+  ../riscv-binutils/configure \
+    --target=${__OPT_TARGET_ARCH} \
+    --prefix=${__OPT_TARGET_PATH} \
+    --with-arch=${__OPT_TARGET_MARCH_FULL} \
+    --program-prefix=${__OPT_TARGET_PREFIX} \
+    --enable-lto \
+    --disable-nls --disable-wchar_t \
+    --enable-initfini-array
+  make all ${__OPT_MULTICORE}
+  make install ${__OPT_MULTICORE}
+fi
+mkdir $__ROOT_DIR/built-binutils | true
 
-mkdir build-gcc && cd build-gcc
-../riscv-gcc/configure \
---target=${__OPT_TARGET_ARCH} \
---prefix=${__OPT_TARGET_PATH} \
-${__OPT_BOOTSTRAP} \
---without-headers --enable-languages=c,c++ \
---with-arch=${__OPT_TARGET_MARCH} --with-abi=${__OPT_TARGET_MABI} \
---enable-lto --enable-multilib \
---disable-nls --disable-wchar_t --disable-threads --disable-libstdcxx \
---enable-initfini-array
-make all-gcc ${__OPT_MULTICORE}
-make install-gcc ${__OPT_MULTICORE}
-cd ..
+# ----------------------------------------------------------------------------
+# gcc
+# ----------------------------------------------------------------------------
 
-mkdir build-newlib && cd build-newlib
-../riscv-newlib/configure \
---target=${__OPT_TARGET_ARCH} \
---with-arch=${__OPT_TARGET_MARCH} --with-abi=${__OPT_TARGET_MABI} \
---prefix=${__OPT_TARGET_PATH} \
---enable-multilib \
---disable-newlib-supplied-syscalls --enable-newlib-nano-malloc \
---enable-newlib-global-atexit --enable-newlib-register-fini \
---disable-newlib-multithread
-make all ${__OPT_MULTICORE}
-make install ${__OPT_MULTICORE}
-cd ..
+cd $__ROOT_DIR
+if [ ! -d built-gcc ]; then
+  if [ ! -f ./riscv-gcc/gcc/config/riscv/t-elf-multilib64 ]; then
+    mv ./riscv-gcc/gcc/config/riscv/t-elf-multilib ./riscv-gcc/gcc/config/riscv/t-elf-multilib64 | true
+    ./riscv-gcc/gcc/config/riscv/multilib-generator ${__OPT_TARGET_MULTILIB} \
+      > ./riscv-gcc/gcc/config/riscv/t-elf-multilib32
+    patch ./riscv-gcc/gcc/config.gcc ./config.gcc.patch
+  fi
 
-export CROSS_COMPILE=${__OPT_TARGET_ARCH}-
-export HOSTCFLAGS="-mabi=${__OPT_TARGET_MABI} -march=${__OPT_TARGET_MARCH}"
-export HOSTCXXFLAGS="-mabi=${__OPT_TARGET_MABI} -march=${__OPT_TARGET_MARCH}"
+  rm -r build-gcc || true
+  mkdir build-gcc && cd build-gcc
+  ../riscv-gcc/configure \
+    --target=${__OPT_TARGET_ARCH} \
+    --prefix=${__OPT_TARGET_PATH} \
+    --program-prefix=${__OPT_TARGET_PREFIX} \
+    ${__OPT_BOOTSTRAP} \
+    --without-headers --enable-languages=c,c++ \
+    --with-arch=${__OPT_TARGET_MARCH} --with-abi=${__OPT_TARGET_MABI} \
+    --enable-lto --enable-multilib \
+    --disable-nls --disable-wchar_t --disable-threads --disable-libstdcxx \
+    --enable-initfini-array
+  make all-gcc ${__OPT_MULTICORE}
+  make install-gcc ${__OPT_MULTICORE}
+fi
+mkdir $__ROOT_DIR/built-gcc | true
 
-cd uclibc++
-# Remove exception handling
-rm ./src/eh_globals.cpp | true
-rm ./src/eh_alloc.cpp | true
-rm ./include/unwind-cxx.h | true
+# ----------------------------------------------------------------------------
+# checking gcc
+# ----------------------------------------------------------------------------
 
-make defconfig
-patch ./.config ../config.uclibc++.patch
+cd $__ROOT_DIR
+if [ ! -d checked-gcc ]; then
+  rm -r check-gcc || true
+  mkdir check-gcc && cd check-gcc
 
-# Patch destination path
-sed -i 's|UCLIBCXX_RUNTIME_PREFIX=""|UCLIBCXX_RUNTIME_PREFIX="'${__OPT_TARGET_PATH}'/'${__OPT_TARGET_ARCH}'"|' .config
+  cat >start.s <<EOF
+.section .text
+_start:
+  nop
+EOF
 
-# Build default version
-make lib
-make install-include install-lib install-bin
-make distclean
+  for type in ${__OPT_TARGET_MULTILIB}; do
+    march=`echo $type | sed 's/\(.*\)-\(.*\)--/\1/'`
+    mabi=`echo $type | sed 's/\(.*\)-\(.*\)--/\2/'`
 
-# Build specific target architectures / ABIs
-for type in ${__OPT_TARGET_MULTILIB}; do
-  march=`echo $type | sed 's/\(.*\)-\(.*\)--/\1/'`
-  mabi=`echo $type | sed 's/\(.*\)-\(.*\)--/\2/'`
+    ${__OPT_TARGET_PREFIX}-gcc --march=$march --mabi=$mabi -o $march.o start.s
+  done
+fi
+mkdir $__ROOT_DIR/checked-gcc | true
+
+# ----------------------------------------------------------------------------
+# newlib (libc)
+# ----------------------------------------------------------------------------
+
+cd $__ROOT_DIR
+if [ ! -d built-newlib ]; then
+  rm -r build-newlib || true
+  mkdir build-newlib && cd build-newlib
+  ../riscv-newlib/configure \
+    --target=${__OPT_TARGET_ARCH} \
+    --with-arch=${__OPT_TARGET_MARCH} --with-abi=${__OPT_TARGET_MABI} \
+    --prefix=${__OPT_TARGET_PATH} \
+    --enable-multilib \
+    --disable-newlib-supplied-syscalls --enable-newlib-nano-malloc \
+    --enable-newlib-global-atexit --enable-newlib-register-fini \
+    --disable-newlib-multithread
+  make all ${__OPT_MULTICORE}
+  make install ${__OPT_MULTICORE}
+fi
+mkdir $__ROOT_DIR/built-newlib | true
+
+# ----------------------------------------------------------------------------
+# uclibc++ (libcpp)
+# ----------------------------------------------------------------------------
+
+function build_uclibcpp() {
+  MAKE_PARAMS='CROSS_COMPILE="${__OPT_TARGET_PREFIX}-"'
+  if [ -n "$1" && -n "$2" ]; then
+    MAKE_PARAMS+=' HOSTCFLAGS="-march=$1 -mabi=$2"'
+    MAKE_PARAMS+=' HOSTCXXFLAGS="-march=$1 -mabi=$2"'
+  done
+  alias make="$MAKE_PARAMS make"
 
   make defconfig
   patch ./.config ../config.uclibc++.patch
 
-  sed -i 's|UCLIBCXX_RUNTIME_PREFIX=""|UCLIBCXX_RUNTIME_PREFIX="'${__OPT_TARGET_PATH}'/'${__OPT_TARGET_ARCH}'"|' .config
-  sed -i 's|UCLIBCXX_RUNTIME_LIB_SUBDIR=".*"|UCLIBCXX_RUNTIME_LIB_SUBDIR="/lib/'$march'/'$mabi'"|' .config
+  sed -i 's|UCLIBCXX_RUNTIME_PREFIX=""|UCLIBCXX_RUNTIME_PREFIX="${__OPT_TARGET_PATH}/${__OPT_TARGET_ARCH}"|' .config
+  if [ -n "$1" && -n "$2" ]; then
+    sed -i 's|UCLIBCXX_RUNTIME_LIB_SUBDIR=".*"|UCLIBCXX_RUNTIME_LIB_SUBDIR="/lib/'$march'/'$mabi'"|' .config
+  fi
 
-  export HOSTCFLAGS="-mabi=$mabi -march=$march"
-  export HOSTCXXFLAGS="-mabi=$mabi -march=$march"
   make lib
-  make install-lib
+  make install-include install-lib install-bin
   make distclean
-done;
-cd ..
+}
 
-# Rename files to lower case
-orig_dir=`pwd`
-cd ${__OPT_TARGET_PATH}/${__OPT_TARGET_ARCH}/lib
-for file in `find . -name libuClibc++.a`; do mv $file `echo $file | tr [:upper:] [:lower:]`; done;
-cd $orig_dir
+cd $__ROOT_DIR
+if [ ! -d built-uclibc++ ]; then
+  cd riscv-uclibc++
+  git reset --hard HEAD
+  git clean -dfx
+
+  # Remove exception handling
+  rm ./src/eh_globals.cpp | true
+  rm ./src/eh_alloc.cpp | true
+  rm ./include/unwind-cxx.h | true
+
+  # Build default version
+  build_uclibcpp('', '')
+
+  # Build specific target architectures / ABIs
+  for type in ${__OPT_TARGET_MULTILIB}; do
+    march=`echo $type | sed 's/\(.*\)-\(.*\)--/\1/'`
+    mabi=`echo $type | sed 's/\(.*\)-\(.*\)--/\2/'`
+    build_uclibcpp($march, $mabi)
+  done;
+
+  # Rename files to lower case
+  cd ${__OPT_TARGET_PATH}/${__OPT_TARGET_ARCH}/lib
+  for file in `find . -name libuClibc++.a`; do
+    mv $file $(dirname $file)/`echo $(basename $file) | tr [:upper:] [:lower:]`
+  done;
+fi
+mkdir $__ROOT_DIR/built-uclibc++ | true
+
+cd $__ROOT_DIR
+echo "Done"
